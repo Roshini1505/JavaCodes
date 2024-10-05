13892a4fffb6de43ff8f3cdb47485e3133436f3d
Roshini <144936385+Roshini1505@users.noreply.github.com>
2024-10-05 12:28:37 +0530

    TimeCountry.java

---
+++ TimeCountry.java
@@ -0,0 +1,21 @@
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.Instant;
+public class countrytime {
+        public static void main(String[] args) {
+            LocalTime lt = LocalTime.now();
+            System.out.println("current time : " + lt);
+
+            Instant i = Instant.now();
+            System.out.println(i);
+
+            for(String val : ZoneId.getAvailableZoneIds())
+            {
+                System.out.println(val);
+            }
+
+            LocalTime zt = LocalTime.now(ZoneId.of("Asia/Singapore"));
+            System.out.println("At Singapore : " + zt);
+        }
+    }
+
